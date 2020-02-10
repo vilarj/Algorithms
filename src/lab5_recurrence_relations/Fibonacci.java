0d705a16b800c844d966b18cba4cd705cb816dca
vilarj <lamaravillasan@outlook.com>
2020-02-10 08:48:41 -0500

    Added the word document of each lab.

--- src/lab5_recurrence_relations/Fibonacci.java
+++ src/lab5_recurrence_relations/Fibonacci.java
@@ -3,14 +3,8 @@ package lab5_recurrence_relations;
 public class Fibonacci {
 
     public static int Fibonacci(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        if (n == 2) {
-            return 2;
-        }
-
-        else
-            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        if (n <= 1) {return n;}
+        if (n == 2) {return 2;}
+        else{return Fibonacci(n - 1) + Fibonacci(n - 2);}
     }
 }
--- src/lab5_recurrence_relations/main.java
+++ src/lab5_recurrence_relations/main.java
@@ -2,7 +2,7 @@ package lab5_recurrence_relations;
 
 public class main extends Fibonacci {
     public static void main (String[] args) {
-        int n = 25;
+        int n = 9;
 
         System.out.println(Fibonacci(n));
     }
