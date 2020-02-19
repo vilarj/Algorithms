68b9a47d11adb20f8757a909d597096dc33a15bb
vilarj <lamaravillasan@outlook.com>
2020-02-19 08:16:25 -0500

    Another way of doing the iterative fibonacci numbers

--- src/lab5_recurrence_relations/Fibonacci.java
+++ src/lab5_recurrence_relations/Fibonacci.java
@@ -9,15 +9,28 @@ public class Fibonacci {
     }
     public static int iterative(int n)
     {
-        int curFib = 1;
-        int prevFib = 1;
-        for(int i=2; i<n; i++)
-        {
-            int temp = curFib;
-            curFib += prevFib;
-            prevFib = temp;
-        }
-        return curFib;
+//        int curFib = 1;
+//        int prevFib = 1;
+//        for(int i=2; i<n; i++)
+//        {
+//            int temp = curFib;
+//            curFib += prevFib;
+//            prevFib = temp;
+//        }
+//        return curFib;
+        /**
+         * Another way of getting the last 5 digits
+         */
+        int f1 = 0, f2 = 1, f3, c = 0;
 
+        do {
+            f3 = f2 + f1;
+            f1 = f2;
+            f2 = f3 % 100000;
+            c++;
+        }
+        while (c <= n);
+        f2 = f2 % 100000;
+        return f2;
     }
 }
