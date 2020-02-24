3964ba8feeaa8d98f466a898e0169c0363229376
vilarj <lamaravillasan@outlook.com>
2020-02-23 23:16:55 -0500

    Implementation progress

--- src/lab7_hashing/HDictionary.java
+++ src/lab7_hashing/HDictionary.java
@@ -1,18 +1,39 @@
 package lab7_hashing;
 
 public class HDictionary<K,E> {
-    private Entry<Integer, String[]> dictionary;
+    private Entry <K, E> [] dictionary;
+    private int size;
 
     public HDictionary(int size) {
         if(primeNumber(size) == true) {
             // the size of dictionary is n --> TODO: set dictionary the size of n
-            System.out.print("Prime number");
+            dictionary = (Entry<K, E>[]) new Object [size];
+            this.size = size;
         }
 
         else {
             System.out.print("Not a prime number");
         }
     }
+
+    // ** Part A
+    private int hash (K key, int i) {
+        int m = size;
+        int c = key.hashCode();
+        return (h1(c) + i * h2(c))%m;
+    }
+
+    private int h1 ( int k) {
+        int m = size;
+        return (k + (m/2) ) % m;
+    }
+
+    private int h2 (int k) {
+        int m = size;
+        return (k % (m - 1));
+    }
+
+    // **
 
     /**
      * The function checks if the
@@ -24,13 +45,21 @@ public class HDictionary<K,E> {
      */
     public boolean primeNumber(int n) {
         boolean prime = true;
-        int r = 0;
+        int remainder = 0;
 
         for (int i = 2; i <= n - 1; i++) {
-            if (n % i == 0) {r++;}
+            if (n % i == 0) {remainder++;}
         }
 
-        if (r > 0) {return !prime;}
+        if (remainder > 0) {return !prime;}
         return prime;
+    }
+
+    private void hashInsert(K key, E element) {
+
+    }
+
+    private void hashSearch (Entry <K, E> element) {
+
     }
 }
