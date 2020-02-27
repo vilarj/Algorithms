0b34fd15f8132e319153eb109d6b6a6e5f2a5e9f
vilarj <lamaravillasan@outlook.com>
2020-02-27 13:00:02 -0500

    Some of part c..

--- src/lab6_hashing/Test.java
+++ src/lab6_hashing/Test.java
@@ -1,9 +1,12 @@
 package lab6_hashing;
 
-public class Test {
-    public static void main (String[] args) {
-        HDictionary<String, String> dic = new HDictionary<>(1);
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Test {
+    public static void main (String[] args) throws FileNotFoundException {
+        HDictionary<String, String> dic = new HDictionary<>(1);
         Entry car = new Entry<>("Toyota", "Red");
 //        Entry car2 = new Entry<>("Honda", "Blue");
 //        Entry car3 = new Entry<>("Benz", "White");
@@ -17,8 +20,26 @@ public class Test {
 //        dic.Insert(car4);
 //        dic.Insert(car5);
 //        dic.Insert(car6);
-
         System.out.println(dic.Search("Toyota").getValue());
         System.out.println(dic.Search("Toyota").getKey());
+
+        // Part C
+        getFile();
+    }
+
+    public static void getFile () {
+        System.out.println("\n====== Part C ======");
+        try (Scanner read = new Scanner(new File("movies.txt"))) {
+            String message = "";
+
+            while (read.hasNextLine()) {
+                message = read.nextLine();
+                System.out.print (message);
+                System.out.println();
+            }
+        }
+        catch (FileNotFoundException ex) {
+            ex.getStackTrace();
+        }
     }
 }
