cf1227ba29a782baaf4371cc6a55392438e2c68f
vilarj <lamaravillasan@outlook.com>
2020-03-18 18:12:12 -0700

    Simplified some lines

--- src/lab8_dynamic_programming/A.java
+++ src/lab8_dynamic_programming/A.java
@@ -1,10 +1,7 @@
 package lab8_dynamic_programming;
 
-import java.util.Scanner;
-
 public class A {
     public static void main (String[] args) {
-        Scanner input = new Scanner(System.in);
         int[][] array = { // Based on the diagram: -1 -> unable to collect, 0 -> empty, 1 -> able to collect
                 {0, -1, 0, 1, 0, 0},
                 {1, 0, 0, -1, 1, 0},
@@ -13,31 +10,32 @@ public class A {
                 {-1, -1, -1, 0, 1, 0}
         };
 
-        System.out.print ("Longest Path: " + Collector(5, 6, array) );
-
-        // Closing the Scanner class
-        input.close();
+        System.out.printf ("Longest Path: %d", Collector(5, 6, array) );
     }
 
     public static int Collector (int rows, int columns, int[][] array) {
-        int[][] position = new int [rows][columns];
-        int i = 1, j = 1, k;
+        int[][] position = new int[rows][columns];
+        int i = 1, j = 1;
 
-        while ( (i < rows) && (array[i][0] != -1) ) {
-            position[i][0] = position[i-1][0] + array[i][0]; // find the location of the coin
+        while ((i < rows) && (array[i][0] != -1)) {
+            position[i][0] = position[i - 1][0] + array[i][0]; // find the location of the coin
             i++;
         }
 
-        for (; i < rows; i++)
+        while (i < rows) {
             position[i][0] = -6;
+            i++;
+        }
 
         while ( (j < columns) && (array[0][j] != -1) ) {
             position[0][j] = position[0][j-1] + array[0][j];
             j++;
         }
 
-        for (; j < columns; j++)
+        while (j < columns) {
             position[0][j] = -6;
+            j++;
+        }
 
         // This part finds the best path for collecting the coins
         for (i = 1; i < rows; i++) {
--- src/lab8_dynamic_programming/B.java
+++ src/lab8_dynamic_programming/B.java
@@ -4,13 +4,15 @@ public class B {
     private static final int MIN_INTEGER = Integer.MIN_VALUE;
 
     /*
+
     Several lengths:
     5, 6, 10, 20
 
     Practice table:
     1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20
     1 	5	7	8	10	16	18	19	25	26	30	38	46	41	50	61	70	75	80	86
-     */
+
+    */
 
     public static int CutRod (int length, int[] list) {
         int maxPrice = MIN_INTEGER;
@@ -26,7 +28,7 @@ public class B {
     }
 
     public static void main (String[] args) {
-        int[] list = new int[] {1, 5, 7, 8, 10, 16, 18,	19,	25,	26,	30,	38,	46,	41,	50,	61,	70,	75,	80,	86};
+        int[] list = {1, 5, 7, 8, 10, 16, 18,	19,	25,	26,	30,	38,	46,	41,	50,	61,	70,	75,	80,	86};
 
         System.out.printf("Max Value: %d%n", CutRod(5, list));
         System.out.printf("Max Value: %d%n", CutRod(6, list));
